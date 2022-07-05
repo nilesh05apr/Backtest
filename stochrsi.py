297c013d8d60c4a8e9d77d3436b76b0d6c73fe47
nilesh05apr <nk8588338@gmail.com>
2022-07-05 12:50:33 +0530

    added analyzers

--- stochrsi.py
+++ stochrsi.py
@@ -7,6 +7,10 @@ import sys  # To find out the script name (in argv[0])
 
 # Import the backtrader platform
 import backtrader as bt
+import backtrader.analyzers as btanalyzers
+import backtrader.feeds as btfeeds
+import backtrader.strategies as btstrats
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -40,8 +44,9 @@ class TestStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
         ''' Logging function fot this strategy'''
+        size = self.getposition().size
         dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+        print('%s, %s CURRENT POSITION %s' % (dt.isoformat(), txt, size))
 
     def __init__(self):
         # Keep a reference to the "close" line in the data[0] dataseries
@@ -56,12 +61,12 @@ class TestStrategy(bt.Strategy):
       sell_signal = previous_stochrsi > current_stochrsi and current_stochrsi > 0.8
 
 
-      if buy_signal and not self.order_exist:
-        self.log('BUY CREATE, %.2f' % self.dataclose[0])
+      if buy_signal:
+        self.log('BUY CREATE AT PRICE , %.2f' % self.dataclose[0])
         self.buy()
         self.order_exist = True
       elif sell_signal and self.order_exist:
-        self.log('SELL CREATE, %.2f' % self.dataclose[0])
+        self.log('SELL CREATE AT PRICE , %.2f' % self.dataclose[0])
         self.sell()
         self.order_exist = False
 
@@ -71,11 +76,34 @@ cb.addstrategy(TestStrategy)
 data = bt.feeds.YahooFinanceCSVData(dataname = 'TATAMOTORS.NS.csv')
 cb.adddata(data)
 cb.broker.setcash(100000.0)
+cb.broker.setcommission(commission=0.001)
+cb.addsizer(bt.sizers.FixedSize, stake=150)
 print('Starting Portfolio Value: %.2f' % cb.broker.getvalue())
+cb.addanalyzer(btanalyzers.SharpeRatio, _name='sharpe_ratio')
+cb.addanalyzer(btanalyzers.Returns, _name='returns')
+cb.addanalyzer(btanalyzers.DrawDown, _name='draw_down')
+thestrats = cb.run()
+thestrat = thestrats[0]
 
-
-cb.run()
+print('\n\n','-'*30,'ANALYSIS','-'*30)
+print('\n')
+sharperatio = thestrat.analyzers.sharpe_ratio.get_analysis()
+sharperatio = list(sharperatio.items())[0]
+print('Sharpe Ratio:', sharperatio[1])
+print('\n',' '*10,'-'*10,'Returns','-'*10)
+returns = thestrat.analyzers.returns.get_analysis()
+returns = list(returns.items())
+for k,v in returns:
+    print(k,':',v)
+print('\n',' '*10,'-'*10,'DrawDowns','-'*10)
+drawdown = thestrat.analyzers.draw_down.get_analysis()
+drawdown = list(drawdown.items())
+for k,v in drawdown:
+    if k == 'max':
+        max = list(v.items())
+        for k2,v2 in max:
+            print(k2,':',v2)
+    else:
+        print(k,':',v)
+print('\n')
 print('Final Portfolio Value: %.2f' % cb.broker.getvalue())
-figure = plt.gcf()
-figure.set_size_inches(10,5)
-cb.plot()[0][0].savefig('StochRSI.png', dpi=300)
